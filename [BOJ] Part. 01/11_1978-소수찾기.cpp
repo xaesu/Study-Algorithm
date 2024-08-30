2e84c5615d0764385b93e43a806cf1d37d3864e7
xaesu <xaesu6@gmail.com>
2024-08-30 23:26:23 +0900

    240830

---
+++ [BOJ] Part. 01/11_1978-소수찾기.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPrime(int num);
+
+int main() {
+    int N, cnt = 0;
+    cin >> N;
+
+    int * num = new int [N];
+
+    // 입력
+    for (int i = 0; i < N; i++) {
+        cin >> num[i];
+    }
+
+    // 소수일 경우 갯수 추가
+    for (int i = 0; i < N; i++) {
+        if (isPrime(num[i])) cnt++;
+    }
+
+    cout << cnt;
+}
+
+// 소수 판별 (1은 소수가 아님)
+bool isPrime(int num) {
+    if (num > 1) {
+        for (int i = 2; i <= sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+    else return 0;
+}
