579c00d7bfdfd95eae4c3632786380823db22224
xaesu <xaesu6@gmail.com>
2024-09-01 23:58:19 +0900

    240901

---
+++ [BOJ] Part. 01/14_9663-N-Queen.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    // 체스판 배열 생성
+    int ** Chess = new int *[N];
+    for(int i = 0; i < N; ++i) {
+        Chess[i] = new int[N];
+    }
+
+    
+}
+
+void NQueen(){
+    // 가로, 세로, 대각선 체크
+    
+}
