f0ce656caf25c018a641befef1c3a2fcf0d03410
xaesu <xaesu6@gmail.com>
2024-09-08 17:03:46 +0900

    240908

---
+++ [BOJ] Part. 01/17_10989-수정렬하기3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void countingSort(vector<int>& arr, int max);
+
+int main()
+{
+    int N,input;
+    cin >> N;
+
+    // 입력 받은 수의 갯수를 count 배열에 저장
+    vector<int> count(10001, 0);
+    for (int i = 0; i < N; i++) {
+        cin >> input;
+        count[input]++;
+    }
+
+    // count 배열을 순차적으로 순회하면서 숫자 출력
+    for (int i = 1; i <= 10000; i++) {
+        while (count[i] > 0) {
+            cout << i << '\n';
+            count[i]--;
+        }
+    }
+}
