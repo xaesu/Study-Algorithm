12049afa3bdb2dd475c6da08f451a18ec76ee1ea
xaesu <xaesu6@gmail.com>
2024-09-19 21:22:14 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);	// 입력 버퍼 비워주기 + 입력 받기
+	ios_base::sync_with_stdio(false);	// 독립 버퍼 사용 (Cpp or C 단독 사용 필수)
+
+	int T;
+	cin >> T;
+
+	vector <int> add(T);
+	int a, b;
+
+	// 입력
+	for (int i = 0; i < T; i++) {
+		cin >> a >> b;
+		add[i] = a + b;
+	}
+
+	// 출력
+	for (int i = 0; i < T; i++) {
+		cout << "Case #" << i+1 << "\: " << add[i] << '\n';
+	}
+}
