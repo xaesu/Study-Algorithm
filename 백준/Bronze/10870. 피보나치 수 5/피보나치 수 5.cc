d0a5029dc3b8da144041368aa880ccc1e5e0bb8f
xaesu <xaesu6@gmail.com>
2024-09-19 21:20:00 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int Fibonacci(int n);
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << Fibonacci(n);
+}
+
+int Fibonacci(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
