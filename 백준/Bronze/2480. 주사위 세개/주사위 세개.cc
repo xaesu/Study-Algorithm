84ba0b503655a5c55e39afa7e2509b3325b4024c
xaesu <xaesu6@gmail.com>
2024-09-19 21:04:03 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int match(int a, int b, int c);
+int prize(int n, int dice);
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	cout << match(a, b, c);
+}
+
+int match(int a, int b, int c) {
+	if (a == b) {
+		if (b == c) return prize(3, a);
+		else return prize(2, a);
+	}
+	else if (a == c) return prize(2, a);
+	else if (b == c) return prize(2, b);
+	else {
+		int max = 0;
+		if (a > b) {
+			if (a > c) max = a;
+			else max = c;
+		}
+		else {
+			if (b > c) max = b;
+			else max = c;
+		}
+		return prize(1, max);
+	}
+}
+
+int prize(int n, int dice) {
+	switch(n) {
+	case 1:
+		return dice * 100;
+	case 2:
+		return 1000 + dice * 100;
+	case 3:
+		return 10000 + dice * 1000;
+	}
+}
