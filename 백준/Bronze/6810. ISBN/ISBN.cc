8c82674b7d709f317c166d651ebdab365221862e
xaesu <xaesu6@gmail.com>
2024-11-10 23:42:29 +0900

    [Bronze IV] Title: ISBN, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6810. ISBN/ISBN.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int sum = 91 + a * 1 + b * 3 + c * 1;
+
+    cout << "The 1-3-sum is " << sum;
+}
